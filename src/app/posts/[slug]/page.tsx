851054d726c2e4902d37084f59215af74325e448
Minjoo <m0522inju@gmail.com>
2025-07-22 21:15:45 +0200

    style: improve style for chart, quote

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -18,8 +18,6 @@ export default async function PostPage(props: Props) {
 
   const recordMap = await fetchNotionPage(notionPageId);
 
-  const paddingX = 'px-4 sm:px-6 md:px-12 lg:px-32 xl:px-80';
-
   return (
     <article>
       <div className='relative h-80 w-full overflow-hidden'>
@@ -27,15 +25,13 @@ export default async function PostPage(props: Props) {
           <Image className='h-full w-full object-cover' src={imageUrl} alt={title} width={760} height={420} priority />
         )}
         <div className='absolute inset-0 bg-black/20' aria-hidden='true' />
-        <div className={`absolute bottom-8 text-left text-white ${paddingX}`}>
+        <div className='absolute bottom-8 px-4 text-left text-white sm:px-6 md:px-12 lg:px-32 xl:px-80'>
           {category && <CategoryButton category={category} />}
           <h1 className='mt-1 text-3xl leading-tight font-bold'>{title}</h1>
           <p className='mt-1 text-sm text-white/80'>{date}</p>
         </div>
       </div>
-      <div className={paddingX}>
-        <NotionPost recordMap={recordMap} />
-      </div>
+      <NotionPost recordMap={recordMap} />
     </article>
   );
 }
