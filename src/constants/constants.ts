da92d9733b3a99b5ed053fb46aae7acfe2dccce8
Minjoo <m0522inju@gmail.com>
2025-07-23 19:35:49 +0200

    feat: add site url

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,3 +1,3 @@
-export const SITE_URL = 'http://localhost:3000';
+export const SITE_URL = 'https://minjoo-dev.vercel.app';
 export const SITE_NAME = 'minjoo.dev';
 export const DEFAULT_OG_IMAGE = `${SITE_URL}/images/logo.png`;
